6266f6a0a1a5b95fb95483ba131032dc56e41d7c
jezhailwood <128369675+jezhailwood@users.noreply.github.com>
2024-12-12 18:41:24 +0000

    Add database connection check

--- app/public/index.php
+++ app/public/index.php
@@ -3,3 +3,11 @@
 $name = 'World';
 
 echo '<h1>Hello, ' . $name . '</h1>';
+
+// Test database connection.
+try {
+    $dbh = new PDO('mysql:host=mysql;dbname=dev_db', 'user', 'secret');
+    echo '<p>Database connection successful.</p>';
+} catch (PDOException $e) {
+    echo '<p>Database connection failed: ' . $e->getMessage() . '.</p>';
+}
